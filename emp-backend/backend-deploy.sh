cfd91f6794cf43be6a1b8d0b45dae7a1b5cf170f
khan <khansession55@gmail.com>
2024-12-27 17:20:47 +0000

    update

--- emp-backend/backend-deploy.sh
+++ emp-backend/backend-deploy.sh
@@ -1,4 +1,4 @@
 # !/bin/bash
 
-kubectl apply  -n default -f emp-backend-deployment.yaml,emp-backend-service.yaml 
+kubectl apply  -n default -f emp-backend-deployment.yaml,emp-backend-service.yaml,emp-backend-cm0-configmap.yaml
 
