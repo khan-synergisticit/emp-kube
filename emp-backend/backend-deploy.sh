f09f8bc27cd49064093ff7a8a1394a5b26ef94fd
Khan <khansession55@gmail.com>
2025-01-02 10:22:04 +0000

    final

--- emp-backend/backend-deploy.sh
+++ emp-backend/backend-deploy.sh
@@ -1,4 +1,4 @@
 # !/bin/bash
 
-kubectl apply  -n default -f emp-backend-deployment.yaml,emp-backend-service.yaml,emp-backend-cm0-configmap.yaml
+kubectl apply  -n default -f emp-backend-deployment.yaml,emp-backend-service.yaml,emp-backend-cm0-configmap.yaml,emp-backend-claim1-persistentvolumeclaim.yaml
 
--- emp-frontend/frontend-deploy.sh
+++ emp-frontend/frontend-deploy.sh
@@ -1,3 +1,3 @@
 # !/bin/bash
 
-kubectl apply  -n default -f emp-frontend-deployment.yaml,emp-frontend-service.yaml
+kubectl apply  -n default -f emp-frontend-deployment.yaml,emp-frontend-service.yaml,emp-frontend-claim1-persistentvolumeclaim.yaml,emp-frontend-cm0-configmap.yaml
